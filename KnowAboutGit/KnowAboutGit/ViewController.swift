1d733b7367659ce1e93916693a6aa7ae05f3d05e
yana.dudareva <dudareva.yana@outlook.com>
2025-01-24 15:58:54 +0300

    add new project

---
+++ KnowAboutGit/KnowAboutGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KnowAboutGit
+//
+//  Created by Yana Dudareva on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KnowAboutGit/KnowAboutGitTests/KnowAboutGitTests.swift
@@ -0,0 +1,17 @@
+//
+//  KnowAboutGitTests.swift
+//  KnowAboutGitTests
+//
+//  Created by Yana Dudareva on 24.01.2025.
+//
+
+import Testing
+@testable import KnowAboutGit
+
+struct KnowAboutGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
